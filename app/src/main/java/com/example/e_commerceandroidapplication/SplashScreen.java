e9dbc48a8c8282c2d6dbc985e7abf9e730aaaa43
Daly <41022166+daly64@users.noreply.github.com>
2021-02-25 11:57:19 +0100

    Setting up SplashScreen

---
+++ app/src/main/java/com/example/e_commerceandroidapplication/SplashScreen.java
@@ -0,0 +1,70 @@
+package com.example.e_commerceandroidapplication;
+
+import android.annotation.SuppressLint;
+import android.content.Context;
+import android.content.Intent;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
+import android.os.Build;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.View;
+import android.widget.TextView;
+
+import androidx.annotation.RequiresApi;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class SplashScreen extends AppCompatActivity {
+
+    TextView tvVersion;
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        //   mettre l'activité en plein ecran
+        fullScreen();
+    }
+
+    @SuppressLint("SetTextI18n")
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.layout_splash_screen);
+        tvVersion = findViewById(R.id.version);
+        tvVersion.setText(getString(R.string.version)+" "+getVersion(this));
+
+        // durée d'affichage du splash screen 3 seconde
+        int SPLASH_DISPLAY_LENGTH = 3000;
+
+        // après un délaie de x secondes ferme le splash screen et affiche la main activity
+        new Handler().postDelayed(() -> {
+            Intent intent = new Intent(SplashScreen.this,MainActivity.class);
+            SplashScreen.this.startActivity(intent);
+            SplashScreen.this.finish();
+        }, SPLASH_DISPLAY_LENGTH);
+
+    }
+
+    // mode plein ecran
+    private void fullScreen() {
+        getWindow().getDecorView().setSystemUiVisibility(
+                View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                        | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                        | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                        | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION // cache la barre de navigation
+                        | View.SYSTEM_UI_FLAG_FULLSCREEN // cache la barre d'etat
+                        | View.SYSTEM_UI_FLAG_IMMERSIVE);
+    }
+
+    // récupére la version de l'application
+    private String getVersion(Context context) {
+        try {
+            PackageInfo pInfo = context.getPackageManager().getPackageInfo(context.getPackageName(), 0);
+//            String version = pInfo.versionName;
+            return pInfo.versionName;
+        } catch (PackageManager.NameNotFoundException e) {
+            e.printStackTrace();
+        }
+        return "1.0";
+    }
+}
